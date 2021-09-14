4aabc74912bd3b13acf69b8a1e3d50802ef01831
Michael Rätzel <viir@viir.de>
2021-09-14 17:41:36 +0000

    Track compiler output to support inspecting difference
    Store the output from the compiler to illustrate how the compiler transforms our app program file tree.
    
    Here is how we arrived at this state:
    First, use the following command to compile one of the example apps:
    
    ```
    elm-fs  compile  https://github.com/elm-fullstack/elm-fullstack/tree/main/implement/example-apps/demo-backend-state
    ```
    
    Below is the output we got from the command:
    
    ```
    Loaded source composition 344b41834e1d6049abde6e5b5b4e3f7c745f19803d880a39762ef146a21a376f from 'https://github.com/elm-fullstack/elm-fullstack/tree/main/implement/example-apps/demo-backend-state'. Starting to compile...
    
    Compilation completed in 9 seconds, resulting in composition 55360020de890d45f908ccdf4c9656be83b0a488db47cdaa7bde1c79c3531626.
    
    Saved compiled composition 55360020de890d45f908ccdf4c9656be83b0a488db47cdaa7bde1c79c3531626 to '*\elm-fullstack\implement\example-apps\elm-fullstack-tool\report\55360020de890d45f908ccdf4c9656be83b0a488db47cdaa7bde1c79c3531626.zip'.
    Saved report to file 'K:\Source\Repos\elm-fullstack\implement\example-apps\elm-fullstack-tool\report\2021-09-14T17-38-21_compile.json'.
    ```
    
    Then, extract the files from `55360020de890d45f908ccdf4c9656be83b0a488db47cdaa7bde1c79c3531626.zip`, overwriting the files here to get a nice diff rendering.

--- implement/example-apps/demo-backend-state/src/CompilationInterface/ElmMake.elm
+++ implement/example-apps/demo-backend-state/src/CompilationInterface/ElmMake.elm
@@ -1,6 +1,8 @@
 module CompilationInterface.ElmMake exposing (..)
+import Base64
+import CompilationInterface.ElmMake.Generated_4703c917
 
 
 elm_make__debug__base64____src_FrontendWeb_Main_elm : String
 elm_make__debug__base64____src_FrontendWeb_Main_elm =
-    "The compiler replaces this value."
+    CompilationInterface.ElmMake.Generated_4703c917.file_as_base64_71fa735c6b52adcb4bfee3dbd187b9701a109e2baa59c7419763edcf3e54a69e
--- implement/example-apps/demo-backend-state/src/CompilationInterface/GenerateJsonCoders.elm
+++ implement/example-apps/demo-backend-state/src/CompilationInterface/GenerateJsonCoders.elm
@@ -2,8 +2,19 @@ module CompilationInterface.GenerateJsonCoders exposing (..)
 
 import Backend.State
 import Json.Encode
+import CompilationInterface.GenerateJsonCoders.Generated_ab2dd9bd
+import Base64
+import Dict
+import Set
+import Json.Decode
+import Json.Encode
+import Bytes
+import Bytes.Decode
+import Bytes.Encode
+import Backend.State
+import ListDict
 
 
 jsonEncodeBackendState : Backend.State.State -> Json.Encode.Value
 jsonEncodeBackendState =
-    always (Json.Encode.string "The compiler replaces this function.")
+    CompilationInterface.GenerateJsonCoders.Generated_ab2dd9bd.jsonEncode_2ea38cf1d1
